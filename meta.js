cbca011475fca9703f7d8a089e50d35817c17ab8
ajeet326 <ajeet326meena@gmail.com>
2026-01-01 04:00:27 +0530

    Major fix: Proper episode grouping with h4 scanning and quality labels

--- meta.js
+++ meta.js
@@ -1,4 +1,4 @@
-// HDHub4u 2.0 Meta Module - Simplified for Rhino compatibility
+// HDHub4u 2.0 Meta Module - Proper Episode Grouping
 
 var headers = {
     "Cookie": "xla=s4t",
@@ -42,7 +42,6 @@ function getMetaData(link, providerContext) {
         // Extract poster
         var image = $("main.page-body img.aligncenter").first().attr("src") ||
             $("img.aligncenter").first().attr("src") || "";
-        console.log("Image:", image ? "found" : "none");
 
         // Extract synopsis
         var synopsis = "Watch " + (title || "content") + " in high quality.";
@@ -70,76 +69,133 @@ function getMetaData(link, providerContext) {
         }
 
         // ============================================
-        // EXTRACT LINKS (using domain patterns like v1)
+        // EXTRACT LINKS WITH PROPER EPISODE GROUPING
         // ============================================
         var linkList = [];
 
-        // Find ALL links to streaming/download providers
+        // Find full pack links first (720p x264, 1080p x264, etc. - non-episode)
+        var packLinks = [];
         var allLinks = container.find("a");
-        var episodeLinks = [];
-        var qualityLinks = [];
-        var otherLinks = [];
-        var seenUrls = {};
 
         for (var i = 0; i < allLinks.length; i++) {
             var anchor = allLinks.eq(i);
             var href = anchor.attr("href") || "";
             var text = anchor.text().trim();
 
-            // Skip non-http and duplicates
-            if (href.indexOf("http") !== 0 || seenUrls[href]) continue;
+            if (href.indexOf("http") !== 0) continue;
 
-            // Only include links to provider domains
-            var isProvider = href.indexOf("gadgetsweb") !== -1 ||
-                href.indexOf("hubstream") !== -1 ||
-                href.indexOf("hubdrive") !== -1 ||
-                href.indexOf("hubcloud") !== -1 ||
-                href.indexOf("hubcdn") !== -1;
-
-            if (!isProvider) continue;
-
-            seenUrls[href] = true;
-            var textUpper = text.toUpperCase();
-
-            // Categorize by text content
-            if (textUpper.indexOf("EPISODE") !== -1 || textUpper.indexOf("EPISOD") !== -1) {
-                episodeLinks.push({ title: text, link: href });
-            }
-            else if (text.indexOf("480") !== -1 || text.indexOf("720") !== -1 ||
-                text.indexOf("1080") !== -1 || text.indexOf("2160") !== -1 ||
-                text.indexOf("4K") !== -1) {
-                qualityLinks.push({ title: text, link: href });
-            }
-            else {
-                otherLinks.push({ title: text || "Download", link: href });
+            // Quality pack links (contain size like [2.6GB])
+            if (text.indexOf("[") !== -1 && text.indexOf("GB]") !== -1) {
+                var isProvider = href.indexOf("gadgetsweb") !== -1 ||
+                    href.indexOf("hubdrive") !== -1 ||
+                    href.indexOf("hubcloud") !== -1;
+                if (isProvider) {
+                    packLinks.push({ title: text, link: href });
+                }
             }
         }
 
-        console.log("Links found - Episodes:", episodeLinks.length, "Quality:", qualityLinks.length, "Other:", otherLinks.length);
-
-        // Build linkList structure
-        if (episodeLinks.length > 0) {
+        // Add pack links as a group
+        if (packLinks.length > 0) {
             linkList.push({
-                title: "Episodes",
-                directLinks: episodeLinks
+                title: "Full Season Pack",
+                directLinks: packLinks
             });
         }
 
-        if (qualityLinks.length > 0) {
-            linkList.push({
-                title: "Quality Options",
-                directLinks: qualityLinks
-            });
+        // Find episode links by scanning h4/strong elements for "EPiSODE X"
+        var html = response.data;
+        var currentEpisode = "";
+        var episodeMap = {}; // { "Episode 1": [{title, link}, ...], ... }
+
+        // Method: Find all h4 elements and track episode context
+        var h4Elements = container.find("h4");
+        console.log("Found h4 elements:", h4Elements.length);
+
+        for (var h = 0; h < h4Elements.length; h++) {
+            var h4 = h4Elements.eq(h);
+            var h4Text = h4.text().trim().toUpperCase();
+
+            // Check if this h4 is an episode header
+            if (h4Text.indexOf("EPISODE") !== -1 || h4Text.indexOf("EPISOD") !== -1) {
+                // Extract episode number
+                var epNum = "";
+                for (var c = 0; c < h4Text.length; c++) {
+                    var ch = h4Text.charAt(c);
+                    if (ch >= "0" && ch <= "9") {
+                        epNum += ch;
+                    } else if (epNum.length > 0) {
+                        break; // Stop after getting digits
+                    }
+                }
+                if (epNum) {
+                    currentEpisode = "Episode " + epNum;
+                    if (!episodeMap[currentEpisode]) {
+                        episodeMap[currentEpisode] = [];
+                    }
+                }
+            }
+            // If we have a current episode, look for links in this h4
+            else if (currentEpisode) {
+                var h4Links = h4.find("a");
+                for (var l = 0; l < h4Links.length; l++) {
+                    var epLink = h4Links.eq(l);
+                    var epHref = epLink.attr("href") || "";
+                    var epText = epLink.text().trim();
+
+                    if (epHref.indexOf("http") !== 0) continue;
+
+                    var isProvider = epHref.indexOf("gadgetsweb") !== -1 ||
+                        epHref.indexOf("hubstream") !== -1 ||
+                        epHref.indexOf("hubdrive") !== -1 ||
+                        epHref.indexOf("hubcloud") !== -1 ||
+                        epHref.indexOf("hubcdn") !== -1;
+
+                    if (isProvider && epText) {
+                        // Get quality from h4 text (720p, 1080p)
+                        var quality = "";
+                        if (h4Text.indexOf("720") !== -1) quality = "720p";
+                        else if (h4Text.indexOf("1080") !== -1) quality = "1080p";
+                        else if (h4Text.indexOf("480") !== -1) quality = "480p";
+
+                        var linkTitle = epText;
+                        if (quality) linkTitle = quality + " - " + epText;
+
+                        episodeMap[currentEpisode].push({
+                            title: linkTitle,
+                            link: epHref
+                        });
+                    }
+                }
+            }
         }
 
-        if (otherLinks.length > 0) {
-            linkList.push({
-                title: "Download Links",
-                directLinks: otherLinks
-            });
+        // Convert episodeMap to linkList
+        var episodeKeys = [];
+        for (var key in episodeMap) {
+            if (episodeMap.hasOwnProperty(key)) {
+                episodeKeys.push(key);
+            }
         }
 
-        console.log("LinkList total:", linkList.length);
+        // Sort episodes numerically
+        episodeKeys.sort(function (a, b) {
+            var numA = parseInt(a.replace(/\D/g, "")) || 0;
+            var numB = parseInt(b.replace(/\D/g, "")) || 0;
+            return numA - numB;
+        });
+
+        for (var e = 0; e < episodeKeys.length; e++) {
+            var epKey = episodeKeys[e];
+            if (episodeMap[epKey].length > 0) {
+                linkList.push({
+                    title: epKey,
+                    directLinks: episodeMap[epKey]
+                });
+            }
+        }
+
+        console.log("LinkList total groups:", linkList.length);
 
         return {
             title: title || "Unknown Title",
